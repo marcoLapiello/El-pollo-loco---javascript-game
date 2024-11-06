623d781b46f30b86d3a86ce5f29a8b934a923519
marcoLapiello <marcolapiello@gmail.com>
2024-11-06 10:35:54 +0100

    Some basic object classes added

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,11 @@
 let canvas;
 let ctx;
-let character = new Image();
+
+let world = new World();
 
 function init() {
-    canvas = document.getElementById("canvas");
-    ctx = canvas.getContext('2d');
-    
-    character.src = '../Grafics/img/2_character_pepe/2_walk/W-21.png';
+  canvas = document.getElementById("canvas");
+  ctx = canvas.getContext("2d");
 
-    setTimeout(() => {
-        ctx.drawImage(character, 50, 200, 100, 210);
-    }, 500);
-    
+  console.log("My Character is", world.character);
 }
