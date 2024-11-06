41c65b6bf64ac4b9b6357c60a7dc9060fb16b4d0
marcoLapiello <marcolapiello@gmail.com>
2024-11-06 15:52:16 +0100

    first background added and code cleaned

--- scripts/models/background.class.js
+++ scripts/models/background.class.js
@@ -1,6 +1,10 @@
 class Background extends MovableObject {
+    width = 720;
+    height = 480;
 
-    constructor(imagePath) {
+    constructor(imagePath, x, y) {
         super().loadImage(imagePath);
+        this.x = x;
+        this.y = y;
     }
 }
--- scripts/models/world.class.js
+++ scripts/models/world.class.js
@@ -2,9 +2,7 @@ class World {
   character = new Character();
   enemies = [new Chicken(), new Chicken(), new Chicken()];
   clouds = [new Clouds()];
-  background = [
-    new Background()
-  ]
+  background = [new Background("Grafics/img/5_background/layers/3_third_layer/1.png", 0, 0)];
   canvas;
   ctx;
 
@@ -17,25 +15,23 @@ class World {
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
+    this.addObjectToMap(this.background);
     this.addToMap(this.character);
+    this.addObjectToMap(this.clouds);
+    this.addObjectToMap(this.enemies);
 
-    this.enemies.forEach((enemy) => {
-        this.addToMap(enemy);
-    });
-
-    this.clouds.forEach((cloud) => {
-        this.addToMap(cloud);
-      });
-
-    let self = this;
     requestAnimationFrame(() => {
-      // this method calls the draw function again and again
-      self.draw(); // "this" is not recognized inside this structure
-      // that s why we set a variable with value "this"
+      this.draw();
     });
   }
 
-  addToMap(movableObject){
+  addObjectToMap(object) {
+    object.forEach((o) => {
+      this.addToMap(o);
+    });
+  }
+
+  addToMap(movableObject) {
     this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
   }
 }
