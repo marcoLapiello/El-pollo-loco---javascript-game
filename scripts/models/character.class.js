9f38e33fe59218e06ba26341d98f288d7f1b68b9
marcoLapiello <marcolapiello@gmail.com>
2024-11-07 12:22:07 +0100

    Main character - walk() added to simulate through the img change, a walking animation

--- scripts/models/character.class.js
+++ scripts/models/character.class.js
@@ -8,14 +8,24 @@ class Character extends MovableObject {
     "Grafics/img/2_character_pepe/2_walk/W-23.png",
     "Grafics/img/2_character_pepe/2_walk/W-24.png",
     "Grafics/img/2_character_pepe/2_walk/W-25.png",
-    "Grafics/img/2_character_pepe/2_walk/W-26.png"
+    "Grafics/img/2_character_pepe/2_walk/W-26.png",
   ];
+  currentImageIndex = 0;
 
   constructor() {
     super().loadImage("Grafics/img/2_character_pepe/2_walk/W-21.png");
     this.loadImages(this.IMAGES_WALKING);
 
-    // this.animate();
+    this.walk();
+  }
+
+  walk() {
+    setInterval(() => {
+      let index = this.currentImageIndex % this.IMAGES_WALKING.length;
+      let path = this.IMAGES_WALKING[index];
+      this.img = this.imageCache[path];
+      this.currentImageIndex++;
+    }, 100);
   }
 
   jump() {}
