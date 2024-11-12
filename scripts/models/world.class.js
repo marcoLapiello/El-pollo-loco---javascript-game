ac43c6850dcd7f0650e2b22c04b58a33ac2c2399
marcoLapiello <marcolapiello@gmail.com>
2024-11-12 12:58:15 +0100

    working on collision between objects - a frame gets added to visualize it better

--- scripts/models/world.class.js
+++ scripts/models/world.class.js
@@ -16,7 +16,6 @@ class World {
     this.draw();
   }
 
-
   draw() {
     this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
@@ -43,33 +42,22 @@ class World {
   addToMap(movableObject) {
     this.ctx.save(); // Salva lo stato corrente del contesto
 
-    // this.ctx.beginPath();
-    // this.ctx.rect(movableObject.x, movableObject.y, movableObject.width, movableObject.height);
-    // this.ctx.strokeStyle = 'red';
-    // this.ctx.lineWidth = 2;
-    // this.ctx.stroke();
-  
+    if (movableObject instanceof Character || movableObject instanceof Chicken || movableObject instanceof Endboss) {
+      this.ctx.beginPath();
+      this.ctx.rect(movableObject.x, movableObject.y, movableObject.width, movableObject.height);
+      this.ctx.strokeStyle = "red";
+      this.ctx.lineWidth = 2;
+      this.ctx.stroke();
+    }
+
     if (movableObject.facingLeft) {
       this.ctx.translate(movableObject.x + movableObject.width, 0); // Trasla il contesto verso destra dell'immagine
       this.ctx.scale(-1, 1); // Riflette l'immagine lungo l'asse x
-      this.ctx.drawImage(
-        movableObject.img,
-        0, // L'immagine ora parte dalla posizione traslata
-        movableObject.y,
-        movableObject.width,
-        movableObject.height
-      );
+      this.ctx.drawImage(movableObject.img, 0, movableObject.y, movableObject.width, movableObject.height);
     } else {
-      this.ctx.drawImage(
-        movableObject.img,
-        movableObject.x,
-        movableObject.y,
-        movableObject.width,
-        movableObject.height
-      );
+      this.ctx.drawImage(movableObject.img, movableObject.x, movableObject.y, movableObject.width, movableObject.height);
     }
-    
-  
+
     this.ctx.restore(); // Ripristina lo stato del contesto originale
   }
 }
