1d34e08402d4863a71e8d33cc8181e44c05ee9f0
marcoLapiello <marcolapiello@gmail.com>
2025-01-18 13:27:59 +0100

    More clouds were added

--- scripts/models/cloud.class.js
+++ scripts/models/cloud.class.js
@@ -9,8 +9,7 @@ export class Clouds extends MovableObject {
 
   constructor() {
     super().loadImage("Grafics/img/5_background/layers/4_clouds/1.png");
-    this.x = Math.random() * 720;
-    // this.registerMovement();
+    this.x = Math.random() * 3876;
   }
 
   registerAnimation() {
